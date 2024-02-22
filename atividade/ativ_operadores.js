ed6853452437cf5f18da2ce78a898bfa87ec7901
Sofia Melo <160147847+MeloSofia@users.noreply.github.com>
2024-02-22 11:32:36 -0400

    Add files via upload

---
+++ atividade/ativ_operadores.js
@@ -0,0 +1,27 @@
+var num1 = 110;
+var num2 = 100;
+adicao = num1 + num2;
+subt = num1 - num2;
+mult = num1 * num2;
+divis = num1 / num2;
+console.log("Adição:", adicao, "Subtração:", subt, "Multiplicação:", mult, "Divisão:", divis)
+
+var num3 = 23;
+var num4 = 16;
+console.log(num3 > num4);
+console.log(num3 < num4);
+
+var idade = 25
+if (idade >= 18) {
+    console.log("Maior de idade")
+} else{
+    console.log("Menor de idade")
+}
+
+num = 5
+if (num % 2 == 0){
+    console.log("Este número é par")
+} else{
+    console.log("Este número é ímpar")
+}
+
---
+++ atividade/ativ_variaveis e tipos.js
@@ -0,0 +1,16 @@
+var nome= "Sofia";
+var idade = 16;
+console.log ("Olá, meu nome é",nome, "e eu tenho",idade);
+
+var maiuscula = "sofia";
+console.log (maiuscula.toUpperCase());
+
+var minuscula = "JÚLIA";
+console.log(minuscula.toLowerCase())
+
+var num = 12;
+console.log(typeof num);
+
+var peso = 54.75
+var altura = 1.59
+console.log(peso/(altura*altura));
