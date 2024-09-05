2c1b77abc1a79f464902dac74140c03792e3fd15
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2024-09-05 09:42:45 -0400

    update

--- API/index.js
+++ API/index.js
@@ -1,8 +1,15 @@
 const express = require  ('express'); 
 const app = express (); 
 
+// Outra forma de declarar
+
+// app.get ('/',(req, res) => { 
+//     req.json({mensagem: 'Hello word'}) 
+// }) ;
+
 app.get ('/',(req, res) => { 
-    res.send ('Hello word') 
+    let mensagem = ({mensagem: 'oi world'}) 
+    res.json(mensagem)
 }) ;
     
 app.listen (8080, () => { 
