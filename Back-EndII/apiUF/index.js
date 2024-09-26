eca476352462c82fb21655ccf96b1e6c0a93eba6
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2024-09-26 11:20:25 -0400

    update

--- Back-EndII/apiUF/index.js
+++ Back-EndII/apiUF/index.js
@@ -2,8 +2,25 @@ const express = require('express');
 const colecaoUF = require('./Dados/dados.js');
 
 const app = express();
-app.get('/ufs', (req, res) =>{
-    res.json(colecaoUF)
+app.get('/ufs/:iduf', (req, res) =>{
+    const iduf = parseInt(req.params.iduf);
+    let mensagemErro = '';
+    let uf;
+
+    if (!(isNaN(iduf))){
+        uf = colecaoUF.colecaoUf.find(u => u.id === iduf);
+        if (!uf){
+            mensagemErro = 'UF não encontrada!'
+        }
+    } else{
+        mensagemErro = 'Requisição inválida!'
+    }
+
+    if (uf){
+        res.json(uf);
+    } else{
+        res.status(404).json({"Erro": mensagemErro})
+    }
 })
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080")
