7d02374c0859dc643e6009bad6d6d17785f01929
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2025-02-13 11:32:48 -0400

    update

--- Back-EndII/apiLibertadores/index.js
+++ Back-EndII/apiLibertadores/index.js
@@ -1,9 +1,49 @@
+import cors from 'cors' //permitindo que rotas externas façam requisição com ele
 import express from 'express';
 import {retornaCampeonatos, retornaCampeonatosAno, retornaCampeonatosTime, retornaCampeonatosID } from './servico/retornaCampeonato_servico.js';
 import { cadastroCampeonato } from './servico/cadastroCampeonato_servico.js';
+import { atualizarCampeonato } from './servico/atualizarCampeonato_servico.js';
+import { AtualizaCampeonatoParcial } from './servico/atualizarCampeonato_servico.js';
 
 const app = express();
+app.use(cors());//permitindo que rotas externas façam requisição com ele
 app.use(express.json());//Suporte para json no corpo da requisição
+
+app.patch('/campeonatos/:id', async (req,res) =>{
+    const {id} = req.params;
+    const{campeao, vice, ano} = req.body;
+    const camposAtualizar = {};
+    if (campeao) camposAtualizar.campeao = campeao;
+    if (vice) camposAtualizar.vice = vice;
+    if (ano) camposAtualizar.ano = ano;
+
+    if (Object.keys(camposAtualizar).length === 0) {
+        res.status(400).send("Nenhum cmapo válido foi enviado para atualização!");
+    } else {
+        const resultado = await AtualizaCampeonatoParcial(id,camposAtualizar);
+        if (resultado.affectedRows > 0) {
+            res.status(202).send('Registro atualizado com sucesso')
+        } else {
+            res.status(404).send('Registro não encontrado!')
+        }
+    }
+})
+
+app.put('/campeonatos/:id', async (req,res) =>{
+    const{id} = req.params;
+    const{campeao, vice, ano} = req.body;
+
+    if (campeao == undefined || vice == undefined || ano == undefined) {
+        res.status(400).send('Nem todos os campos foram informados')
+    } else {
+        const resultado = await atualizarCampeonato(id, campeao, vice, ano);
+        if (resultado.affectedRows >0) {
+            res.status(202).send('Registro atualizado com sucesso!')
+        } else {
+            res.status(400).send('Registro não encontrado!')
+        }
+    }
+})
 
 app.post('/campeonatos', async (req,res) =>{
     const campeao =req.body.campeao;
